9012c709d44905ff6c872d2e8a7a36bab1bff63c
kkeval <kevalpanchal58@gmail.com>
2021-02-10 14:43:52 +0530

    Nav removed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import MainPage from "./components/MainPage";
-import NavBar from "./components/NavBar";
+
 import Problems from "./components/Problems.js";
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
+     
       <Switch>
         <Route path="/Home" exact component={() => <MainPage />} />
         <Route path="/" exact component={() => <MainPage />} />
